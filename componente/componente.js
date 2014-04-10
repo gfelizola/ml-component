873834d41c57496755c4d69c47017fe6907d768f
Gustavo Felizola <gustavo.felizola@tribointeractive.com.br>
2014-04-10 00:49:23 -0300

    separando e documentando componente

---
+++ componente/componente.js
@@ -0,0 +1,244 @@
+
+    /**
+    * Classe que utiliza os serviços do MapLink JS API.
+    * @class
+    */
+    function RouterInformer () {
+        /**
+        * Serviço fornecido pelo MapLink JS API. 
+        * É utilizada para consumir o serviço SOAP “AddressFinder”
+        * @property {MWsAddressFinder}
+        */
+        this.wsEndereco     = new MWsAddressFinder();
+
+        /**
+        * Serviço fornecido pelo MapLink JS API. 
+        * É utilizada para consumir o serviço SOAP “Route”
+        * @property {MWsRoute}
+        */
+        this.wsRota         = new MWsRoute();
+
+        /**
+        * Objejo fornecido pelo MapLink JS API, para utilizar no serviço de endereço (MWsAddressFinder). 
+        * @property {MAddressOptions}
+        */
+        this.endOpcoes      = new MAddressOptions();
+
+        /**
+        * Objejo fornecido pelo MapLink JS API, para utilizar no serviço de rota (MWsRoute). 
+        * @property {MVehicle}
+        */
+        this.veiculo        = new MVehicle();
+
+        /**
+        * Objejo fornecido pelo MapLink JS API, para utilizar no serviço de rota (MWsRoute). 
+        * @property {MRouteDetails}
+        */
+        this.rotaDetalhes   = new MRouteDetails();
+
+        /**
+        * Objejo fornecido pelo MapLink JS API, para utilizar no serviço de rota (MWsRoute). 
+        * @property {MRouteOptions}
+        */
+        this.rotaOpcoes     = new MRouteOptions();
+
+        /**
+        * Lista dos endereços armazenados para uso nos métodos do RouterInformer
+        * @property {Array}
+        */
+        this.listaEnderecos = [];
+
+        //configura padroes
+        this.configurarVeiculo();
+        this.configurarRotaDetalhes();
+        this.configurarRotaOpcoes();
+        this.configurarEnderecoOpcoes(true);
+    }
+
+    /*===== Getters / Setters =====*/
+
+    /**
+    * Método para trocar o veículo utilizado
+    * @param {MVehicle} [pVeiculo]
+    */
+    RouterInformer.prototype.setVeiculo = function(pVeiculo) { this.veiculo = pVeiculo; };
+
+    /**
+    * Método que retorna o veículo utilizado
+    * @return {MVehicle} veículo utilizado na rota
+    */
+    RouterInformer.prototype.getVeiculo = function() { return this.veiculo; };
+
+    /**
+    * Método para trocar o objeto de detalhes de rota
+    * @param {MRouteDetails} [pRotaDetalhes]
+    */
+    RouterInformer.prototype.setRotaDetalhes = function(pRotaDetalhes) { this.rotaDetalhes = pRotaDetalhes; };
+
+    /**
+    * Método que retorna os detalhes de rota
+    * @return {MRouteDetails} objeto de detalhes de rota
+    */
+    RouterInformer.prototype.getRotaDetalhes = function() { return this.rotaDetalhes; };
+
+    /**
+    * Método para trocar o objeto de opções de rota
+    * @param {MRouteOptions} [pRotaOpcoes]
+    */
+    RouterInformer.prototype.setRotaOpcoes = function(pRotaOpcoes) { this.rotaOpcoes = pRotaOpcoes; };
+
+    /**
+    * Método que retorna as opções de rota
+    * @return {MRouteOptions} objeto de opções de rota
+    */
+    RouterInformer.prototype.getRotaOpcoes = function() { return this.rotaOpcoes; };
+
+    /**
+    * Método para trocar o objeto de opções de endereço
+    * @param {MAddressOptions} [pEnderecoOpcoes]
+    */
+    RouterInformer.prototype.setEnderecoOpcoes = function(pEnderecoOpcoes) { this.endOpcoes = pEnderecoOpcoes; };
+
+    /**
+    * Método que retorna as opções de endereço
+    * @return {MAddressOptions} objeto de opções de endereço
+    */
+    RouterInformer.prototype.getEnderecoOpcoes = function() { return this.endOpcoes; };
+
+    /**
+    * Método que retorna a lista de endereços
+    * @return {Array} lista de endereços
+    */
+    RouterInformer.prototype.getEnderecos = function() { return this.listaEnderecos; };
+
+    /**
+    * Função para configurar Veiculo
+    * @param {number} [capacidadeTanque=20] Capacidade do tanque do veículo
+    * @param {number} [consumoMedio=9] Consumo médio do veículo
+    * @param {number} [precoCombustivel=3] Preço do litro de combustível
+    * @param {number} [velocidadeMedia=60] Velocidade do veículo
+    * @param {number} [categoriaPedagio=2] Categoria de pedágio usada no cálculo de rota
+    */
+    RouterInformer.prototype.configurarVeiculo = function(capacidadeTanque, consumoMedio, precoCombustivel, velocidadeMedia, categoriaPedagio) {
+        this.veiculo.tankCapacity       = capacidadeTanque  || 20;
+        this.veiculo.averageConsumption = consumoMedio      || 9;
+        this.veiculo.fuelPrice          = precoCombustivel  || 3;
+        this.veiculo.averageSpeed       = velocidadeMedia   || 60;
+        this.veiculo.tollFeeCat         = categoriaPedagio  || 2;
+    };
+
+    /** Função para configurar detalhes da Rota 
+    * @param {boolean} [otimizarRota=true] Otimizar a rota
+    * @param {number} [tipoRota=0] Tipo de rota utilizada
+    * @param {number} [tipoDescricao=0] Tipo de descrição
+    */
+    RouterInformer.prototype.configurarRotaDetalhes = function(otimizarRota, tipoRota, tipoDescricao) {
+        this.rotaDetalhes.optimizeRoute   = !!otimizarRota;
+        this.rotaDetalhes.descriptionType = tipoDescricao   || 0;
+        this.rotaDetalhes.routeType       = tipoRota        || 0;
+    };
+
+    /** Função para configurar Opções da Rota
+    * @param {string} [lingua=portugues] lingua utilizada no retorno
+    * @param {MRouteOptions} [pRotaDetalhes=this.rotaDetalhes] Tipo de rota utilizada
+    * @param {nMVehicle} [pVeiculo=this.veiculo] Tipo de descrição
+    */
+    RouterInformer.prototype.configurarRotaOpcoes = function(lingua, pRotaDetalhes, pVeiculo) {
+        this.rotaOpcoes.language     = lingua || "portugues";
+        this.rotaOpcoes.routeDetails = pRotaDetalhes || this.rotaDetalhes;
+        this.rotaOpcoes.vehicle      = pVeiculo || this.veiculo;
+    };
+
+    /** Função para configurar Opções de Endereço
+    * @param {boolean} [usarFonetica=true] Otimizar a rota
+    * @param {number} [tipoBusca=2] Tipo de busca utilizada
+    * @param {number} [tipoAcerto=1] Tipo de Match utilizado na busca
+    */
+    RouterInformer.prototype.configurarEnderecoOpcoes = function(usarFonetica, tipoBusca, tipoAcerto) {
+        this.endOpcoes.matchType   = tipoAcerto || 1;
+        this.endOpcoes.searchType  = tipoBusca || 2;
+        this.endOpcoes.usePhonetic = !!usarFonetica;
+    };
+
+    /** Função Helper para transformar objeto MAddress em um formato legível e humano
+    * @param {MAddress} objeto de endereço que será tratado
+    * @return {string} string com endereço em formato legível e humano
+    */
+    RouterInformer.prototype.humanizarEndereco = function(endereco) {
+        return endereco.street + ", " + endereco.houseNumber + " - " + endereco.city.name + ", " + endereco.city.state;
+    };
+
+    /** Função Helper para juntar parametros em um objeto MAddress
+    * @param {string} [rua] Nome da rua ou avenida
+    * @param {string|number} [numero] Número ou altura da rua
+    * @param {string} [cidade] Nome da cidade
+    * @param {string} [estado] Estado da União
+    * @return {MAddress} objeto de endereço
+    */
+    RouterInformer.prototype.formatarEndereco = function(rua, numero, cidade, estado) {
+        var endereco = new MAddress();
+        endereco.street = rua;
+        endereco.houseNumber = numero;
+
+        var city = new MCity();
+        city.name = cidade;
+        city.state = estado.toUpperCase();
+
+        endereco.city = city;
+        return endereco;
+    };
+
+    /** Função para adicionar um endereco na lista que será usada nas infos de rota
+    * @param {MAddress} [pEndereco] Objeto de endereço
+    * @param {MPoint} [pCoordenadas] Objeto de endereço
+    */
+    RouterInformer.prototype.adicionarEndereco = function(pEndereco, pCoordenadas) {
+        this.listaEnderecos.push( {addr: pEndereco, coords: pCoordenadas } );
+    };
+
+    /** Função para remover um endereco da lista que será usada nas infos de rota
+    * @param {number} [indice] indice do objeto na lista
+    */
+    RouterInformer.prototype.removerEndereco = function(indice) {
+        this.listaEnderecos.splice(indice,1);
+    };
+
+    /** Função que realiza busca das informações do endereco
+    * @param {MAddress} [mEndereco] endereço que será buscado
+    * @callback funcaoRetorno
+    */
+    RouterInformer.prototype.buscarEndereco = function(mEndereco, funcaoRetorno) {
+        var ri = this;
+        this.wsEndereco.findAddress(mEndereco, this.endOpcoes, function(resposta) {
+            funcaoRetorno.apply(ri, [resposta]);
+        });
+    };
+
+    /** Função que realiza busca nas informoções da rota
+    * @callback funcaoRetorno
+    */
+    RouterInformer.prototype.buscarInfosRotas = function(funcaoRetorno) {
+        var ri = this;
+        
+
+        if( this.listaEnderecos.length == 0 ){
+            throw new Error("A lista de endereços tem que possuir itens antes de realizar a busca");
+        } else {
+            var rss = [];
+
+            for (var i = 0; i < this.listaEnderecos.length; i++) {
+                var rs = new MRouteStop();
+                rs.description = this.humanizarEndereco( this.listaEnderecos[i].addr )
+                rs.point = new MPoint( this.listaEnderecos[i].coords.x, this.listaEnderecos[i].coords.y );
+                rss.push(rs)
+            };
+
+            this.wsRota.getRouteTotals(rss, this.rotaOpcoes, function(resposta) {
+                funcaoRetorno.apply(ri, [resposta]);
+            });
+        }
+
+        
+    };
+
+    
--- teste/js/script.js
+++ teste/js/script.js
@@ -13,9 +13,6 @@ $(function() {
     var tplAlerta                = '<div class="alert alert-{1} alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>{0}</div>';
     var tplLinhaEndereco         = '<tr><td>{0}</td><td align="right"><button type="button" ref="{1}" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-trash"></span> Remover</button></td></tr>';
 
-    var enderecoService          = new MWsAddressFinder();
-    var rotaService              = new MWsRoute();
-
     //Objetos da tela
     var btnAddEndereco           = $("#btnAdicionar");
     var btnLimpar                = $("#btnLimpar");
@@ -33,58 +30,28 @@ $(function() {
     var alertas                  = $("#containerAlertas");
     var containerResultados      = $("#resultContainer");
 
-    var listaEnderecos           = [];
-
-    //Defaults
-    var resultRange              = new MResultRange();
-    resultRange.pageIndex        = 1;
-    resultRange.recordsPerPage   = 10;
-
-    var endOpcoes                = new MAddressOptions();
-    endOpcoes.matchType          = 1;
-    endOpcoes.searchType         = 2;
-    endOpcoes.usePhonetic        = true;
-    endOpcoes.resultRange        = resultRange;
-
-    var rotaDetalhes             = new MRouteDetails();
-    rotaDetalhes.optimizeRoute   = true;
-    rotaDetalhes.descriptionType = 0;
-    rotaDetalhes.routeType       = 0; 
-     
-    var veiculo                  = new MVehicle();
-    veiculo.tankCapacity         = 20;
-    veiculo.averageConsumption   = 9;
-    veiculo.fuelPrice            = 3;
-    veiculo.averageSpeed         = 60;
-    veiculo.tollFeeCat           = 2;
-
-    var rotaOpcoes               = new MRouteOptions();
-    rotaOpcoes.language          = "portugues";
-    rotaOpcoes.routeDetails      = rotaDetalhes;
-    rotaOpcoes.vehicle           = veiculo;
+    //Componente
+    var rtInformer = new RouterInformer();
 
     //funcoes
     //funcão que atualiza a tabela de endereços
     function atualizarListaEnderecos () {
         tabela.empty();
 
-        for (var i = 0; i < listaEnderecos.length; i++) {
-            var endereco = listaEnderecos[i].addr;
-            tabela.append( tplLinhaEndereco.format( enderecoParaString(endereco), i ) );
+        var lista = rtInformer.getEnderecos();
+
+        for (var i = 0; i < lista.length; i++) {
+            var endereco = lista[i].addr;
+            tabela.append( tplLinhaEndereco.format( rtInformer.humanizarEndereco( endereco ), i ) );
         };
 
         tabela.find('.btn').click(function(ev) {
             prevEvent(ev);
 
             var indice = $(this).attr('ref');
-            listaEnderecos.splice(indice,1);
+            rtInformer.removerEndereco(indice);
             atualizarListaEnderecos();
         });
-    }
-
-    //função helper para criar uma string a partir de um endereço
-    function enderecoParaString (endereco) {
-        return endereco.street + ", " + endereco.houseNumber + " - " + endereco.city.name + ", " + endereco.city.state;
     }
 
     //funcão para mostrar um alerta na página
@@ -103,55 +70,11 @@ $(function() {
         if(ev) ev.preventDefault();
     }
 
-    // função que realiza a busca de coordenadas de um endereço
-    function buscarCoordenadas(endereco) {
-        enderecoService.findAddress(endereco, endOpcoes, function(resposta) {
-            if( resposta.recordCount <= 0 ){
-                mostrarAlerta("O endereço não foi encontrado", false);
-            } else if( resposta.recordCount > 1 ){
-                mostrarAlerta("O endereço não foi encontrado", false);
-            } else if( resposta.recordCount == 1 ){
-                mostrarAlerta("Endereço adicionado com sucesso", true);
-
-                listaEnderecos.push( {addr: endereco, coords: resposta.addressLocation[0].point } );
-                atualizarListaEnderecos();
-
-                formulario[0].reset();
-                $("#loadingCoords").addClass('hidden');
-            }
-        });
-    }
-
-    //função para buscar as informações das rotas
-    function buscarInfosRotas () {
-        var rss = [];
-
-        for (var i = 0; i < listaEnderecos.length; i++) {
-            var rs = new MRouteStop();
-            rs.description = enderecoParaString( listaEnderecos[i].addr )
-            rs.point = new MPoint(listaEnderecos[i].coords.x, listaEnderecos[i].coords.y);
-            rss.push(rs)
-        };
-
-        rotaOpcoes.routeDetails.routeType     = $("#formOpcoesBusca input[type='radio']:checked").val();
-        rotaOpcoes.vehicle.averageConsumption = $("#txtConsumo").val();
-        rotaOpcoes.vehicle.fuelPrice          = $("#txtPreco").val();
-
-        rotaService.getRouteTotals(rss, rotaOpcoes, function(resposta) {
-            console.log( resposta );
-        })
-    }
-
-    //função para atualizar o painel de resultado
-    function atualizarResultados () {
-        
-    }
-
     //Evento para limpar a tabela de endereços
     btnLimpar.click(function(ev) {
         prevEvent(ev);
 
-        listaEnderecos = [];
+        rtInformer.listaEnderecos = [];
         atualizarListaEnderecos();
     });
 
@@ -182,24 +105,25 @@ $(function() {
         else if( cidade == "" || cidade.length < 3 ){ mostrarAlerta("O campo Cidade é obrigatório"); }
         else if( estado == "" || estado.length < 2 || estado == "0" ){ mostrarAlerta("O campo Estado é obrigatório"); }
         else { 
-
             //tudo ok, adicionar valores
-            var endereco = new MAddress();
-            endereco.street = nome;
-            endereco.houseNumber = numero;
-
-            var city = new MCity();
-            city.name = cidade;
-            city.state = estado.toUpperCase();
-
-            endereco.city = city;
-
-            estado.toUpperCase()
-
-            // listaEnderecos.push( endereco );
-            // atualizarListaEnderecos();
             $("#loadingCoords").removeClass('hidden');
-            buscarCoordenadas(endereco);
+
+            var endereco = rtInformer.formatarEndereco( nome, numero, cidade, estado );
+
+            //buscar informações do endereço
+            rtInformer.buscarEndereco( endereco, function( resposta ) {
+                if( resposta.recordCount <= 0 ){
+                    mostrarAlerta("O endereço não foi encontrado", false);
+                } else {
+                    mostrarAlerta("Endereço adicionado com sucesso", true);
+
+                    rtInformer.adicionarEndereco( endereco, resposta.addressLocation[0].point );
+                    atualizarListaEnderecos();
+
+                    formulario[0].reset();
+                    $("#loadingCoords").addClass('hidden');
+                }
+            });
         }
     });
 
@@ -207,16 +131,31 @@ $(function() {
         prevEvent(ev);
 
         formulario.addClass('hidden');
+        $("#loadingInfo").removeClass('hidden');
         containerResultados.empty();
 
-        if( listaEnderecos.length < 1 ){
-            mostrarAlerta("Informe os endereços para calcular a rota", false);
-        } else {
-            buscarInfosRotas();
-        }
+        rtInformer.configurarRotaDetalhes( true, $("#formOpcoesBusca input[type='radio']:checked").val() );
+        rtInformer.configurarVeiculo( null, $("#txtConsumo").val(), $("#txtPreco").val() );
+
+        rtInformer.buscarInfosRotas(function(resposta) {
+            $("#loadingInfo").addClass('hidden');
+            console.log( "rtInformer.buscarInfosRotas", resposta );
+            atualizarResultados( resposta.totalTime, resposta.totalDistance, resposta.totalfuelCost, resposta.totaltollFeeCost );
+        });
     });
 
-    //temp
+    //função para atualizar o painel de resultado
+    function atualizarResultados(tempoTotal, distanciaTotal, custoCombustivel, custoPedagio) {
+        var tempo = new Date(tempoTotal);
+        console.log(tempo);
+
+        containerResultados.append('<p><b>Tempo total da rota:</b> ' + tempoTotal + '</p>');
+        containerResultados.append('<p><b>Distância total:</b> ' + distanciaTotal + '</p>');
+        containerResultados.append('<p><b>Custo de combustível:</b> ' + custoCombustivel + '</p>');
+        containerResultados.append('<p><b>Custo total considerando pedágio:</b> ' + custoPedagio + '</p>');
+    }
+
+    //Usado para facilitar testes
     $("#btnRua1").click(function(ev) {
         prevEvent(ev);
 
